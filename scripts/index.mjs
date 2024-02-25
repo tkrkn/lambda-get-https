80e142b2992cb7b8bdd67b5c26266484247a5c58
tkrkn <little.cats.and.little.dogs@gmail.com>
2024-02-25 18:50:02 +0900

    prettier導入

--- scripts/index.mjs
+++ scripts/index.mjs
@@ -1,4 +1,4 @@
-import * as https from 'node:https';
+import * as https from "node:https";
 
 /**
  * Get request.
@@ -7,73 +7,77 @@ import * as https from 'node:https';
  * @return {Promise<string>} promise of request
  */
 function get(uri) {
-    return new Promise((resolve, reject) => {
-        https
-            .get(uri, (response) => {
-                // レスポンスのパラメータ
-                const { statusCode, headers } = response;
-                console.debug("Status Code : " + statusCode);
-                console.debug("Header : " + Object.keys(headers).map((key) => key + ":" + headers[key]).join("\n"));
+  return new Promise((resolve, reject) => {
+    https
+      .get(uri, (response) => {
+        // レスポンスのパラメータ
+        const { statusCode, headers } = response;
+        console.debug("Status Code : " + statusCode);
+        console.debug(
+          "Header : " +
+            Object.keys(headers)
+              .map((key) => key + ":" + headers[key])
+              .join("\n"),
+        );
 
-                response.setEncoding('utf8');
-                let responseBody = '';
+        response.setEncoding("utf8");
+        let responseBody = "";
 
-                // レスポンスデータの
-                response.on('data', (chunk) => {
-                    responseBody += chunk;
-                });
+        // レスポンスデータの
+        response.on("data", (chunk) => {
+          responseBody += chunk;
+        });
 
-                response.on('end', () => {
-                    resolve(responseBody);
-                });
-            })
-            .on('error', (error) => {
-                reject(error);
-            });
-    });
+        response.on("end", () => {
+          resolve(responseBody);
+        });
+      })
+      .on("error", (error) => {
+        reject(error);
+      });
+  });
 }
 
 export const handler = async (event) => {
-    // 環境変数
-    /** @type string url */
-    const url = process.env['URL'];
+  // 環境変数
+  /** @type string url */
+  const url = process.env.URL;
 
-    console.log("URL : " + url);
-    if (url === undefined) {
-        throw new Error("URL is undefined.");
-    }
+  console.log("URL : " + url);
+  if (url === undefined) {
+    throw new Error("URL is undefined.");
+  }
 
-    const now = new Date();
-    console.log('Now : ' + now.toString());
+  const now = new Date();
+  console.log("Now : " + now.toString());
 
-    // リクエストをするクエリ
-    const query_hash = {
-        // data: event.data,
-        // time: now.toISOString(),
-        zipcode: "1000001"
-    };
-    // URIを使うためにクエリ文字列に
-    const query_string = Object.keys(query_hash)
-        .map(key => {
-            return key + "=" + encodeURIComponent(query_hash[key]);
-        })
-        .join('&');
-    console.log('Requset QueryStrings : ' + query_string);
+  // リクエストをするクエリ
+  const queryHash = {
+    // data: event.data,
+    // time: now.toISOString(),
+    zipcode: "1000001",
+  };
+  // URIを使うためにクエリ文字列に
+  const queryString = Object.keys(queryHash)
+    .map((key) => {
+      return key + "=" + encodeURIComponent(queryHash[key]);
+    })
+    .join("&");
+  console.log("Requset QueryStrings : " + queryString);
 
-    // クエリ文字列を元にリクエストするURIを作成する
-    const uri = url + '?' + query_string;
-    console.log('Requset URI : ' + uri);
+  // クエリ文字列を元にリクエストするURIを作成する
+  const uri = url + "?" + queryString;
+  console.log("Requset URI : " + uri);
 
-    let response = '';
-    try {
-        response = await get(uri)
-            .catch((error) => {
-                // GETがrejectされた場合
-                response = '';
-            });
-    } catch (e) {
-        // GETでErrorがthrowされた場合
-        response = '';
-    }
-    console.log('Response Data : ' + response);
+  let response = "";
+  try {
+    response = await get(uri).catch(() => {
+      // GETがrejectされた場合
+      response = "";
+    });
+  } catch (e) {
+    // GETでErrorがthrowされた場合
+    response = "";
+  }
+  console.log("Response Data : " + response);
 };
--- tests/units/index.mjs
+++ tests/units/index.mjs
@@ -1,24 +1,23 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import { handler } from "../../scripts/index.mjs";
 
 dotenv.config({
-    path: './tests/.env'
+  path: "./tests/.env",
 });
 
 const event = {
-    "key1": "value1",
-    "key2": "value2",
-    "key3": "value3"
+  key1: "value1",
+  key2: "value2",
+  key3: "value3",
 };
 console.debug("EVENT : " + typeof event, event);
 
 try {
-    const result = handler(event);
+  const result = handler(event);
 
-    result
-        .then((value) => console.log(value))
-        .catch((reason) => console.error(reason))
-        ;
+  result
+    .then((value) => console.log(value))
+    .catch((reason) => console.error(reason));
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
